dd37646561e4e54da045ba8dafcae0e07010363a
Stefano <stefano.dellolioprada@ottogroup.com>
2024-01-30 20:35:22 +0100

    cambios y titulo de restaurante

--- src/components/headhero/Headhero.tsx
+++ src/components/headhero/Headhero.tsx
@@ -5,37 +5,39 @@
  */
 import Image from "next/image";
 import BackgroundImage from "./bruschetta.png";
-import BackgroundImage2 from "./bruschetta.png";
+import BackgroundImage2 from "./crush2.png";
 
 export default function HeadHero() {
     return ( 
         <>
-            <div className="relative w-full h-80">
+            <div className="relative w-full h-60">
                 <Image
-                alt="Background"
-                className="absolute object-cover w-full h-full"
-                height={1080}
-                src={BackgroundImage}
-                style={{
-                    aspectRatio: "1920/1080",
-                    objectFit: "cover",
-                }}
-                width={1920}
+                    alt="Background"
+                    className="absolute object-cover w-full h-full rounded-2xl"
+                    height={1080}
+                    src={BackgroundImage}
+                    style={{
+                        aspectRatio: "1920/1080",
+                        objectFit: "cover",
+                    }}
+                    width={1920}
                 />
                 <div className="absolute inset-0 flex items-center justify-center">
-                <Image
-                    alt="Logo"
-                    className="rounded-full object-contain shadow-lg"
-                    height={200}
-                    src={BackgroundImage2}
-                    style={{
-                    aspectRatio: "200/200",
-                    objectFit: "cover",
-                    }}
-                    width={200}
-                />
+                    <Image
+                        alt="Logo"
+                        className="rounded-full object-contain shadow-lg"
+                        height={160}
+                        src={BackgroundImage2}
+                        style={{
+                        aspectRatio: "100/100",
+                        objectFit: "cover",
+                        }}
+                        width={160}
+                    />
                 </div>
             </div>
+            <h2 className="bg-white pt-2 pb-2 text-lg text-black font-bold font-sans">Restaurante Crush Burgers</h2>
+            <hr />
         </>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
